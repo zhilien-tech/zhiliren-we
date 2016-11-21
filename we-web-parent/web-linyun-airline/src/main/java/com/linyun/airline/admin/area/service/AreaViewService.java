0a358599a5f1925e2a9b196f06347cb97415ab30
Cjb <Cjb@Cuijianbin>
2016-11-21 10:13:15 +0800

    生成区域和部门模块的moudle和service以及页面

---
+++ we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/admin/area/service/AreaViewService.java
@@ -0,0 +1,14 @@
+package com.linyun.airline.admin.area.service;
+
+import com.uxuexi.core.web.base.service.BaseService;
+import com.linyun.airline.entities.TAreaEntity;
+
+import org.nutz.ioc.loader.annotation.IocBean;
+import org.nutz.log.Log;
+import org.nutz.log.Logs;
+
+@IocBean
+public class AreaViewService extends BaseService<TAreaEntity> {
+	private static final Log log = Logs.get();
+   
+}
