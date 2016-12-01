b342bf364a1ef33fa7e7f7a71f8721c1ae4e1538
朱晓川 <3065805295@qq.com>
2016-12-01 19:35:43 +0800

    处理Sabre高级查询接口InstalFlights请求部分

--- we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/common/util/HttpClientUtil.java
+++ we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/common/util/HttpClientUtil.java
@@ -7,10 +7,12 @@
 package com.linyun.airline.common.util;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.net.SocketTimeoutException;
 import java.security.GeneralSecurityException;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
+import java.util.List;
 
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLException;
@@ -32,8 +34,15 @@ import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.apache.http.util.EntityUtils;
+import org.nutz.lang.Mirror;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
+
+import com.google.common.base.Function;
+import com.google.common.base.Joiner;
+import com.google.common.collect.Lists;
+import com.uxuexi.core.common.util.CollectionUtil;
+import com.uxuexi.core.common.util.Util;
 
 /**
  * http请求工具类
@@ -48,12 +57,15 @@ public class HttpClientUtil {
 	private static Log log = Logs.getLog(HttpClientUtil.class);
 
 	private static PoolingHttpClientConnectionManager connMgr;
+
 	private static RequestConfig requestConfig;
+
 	private static final int MAX_TIMEOUT = 10000;
 
 	static {
 		// 设置连接池  
 		connMgr = new PoolingHttpClientConnectionManager();
+
 		// 设置连接池大小  
 		connMgr.setMaxTotal(100);
 		connMgr.setDefaultMaxPerRoute(connMgr.getMaxTotal());
@@ -96,6 +108,7 @@ public class HttpClientUtil {
 
 		} catch (Exception e) {
 			log.error("http get throw Exception");
+			e.printStackTrace();
 
 		} finally {
 			httpPost.abort();
@@ -116,7 +129,6 @@ public class HttpClientUtil {
 
 			HttpEntity entity = response.getEntity();
 			result = EntityUtils.toString(entity, "UTF-8");
-			log.info(result);
 		} catch (ConnectionPoolTimeoutException e) {
 			log.error("http get throw ConnectionPoolTimeoutException(wait time out)");
 
@@ -128,7 +140,7 @@ public class HttpClientUtil {
 
 		} catch (Exception e) {
 			log.error("http get throw Exception");
-
+			e.printStackTrace();
 		} finally {
 			httpGet.abort();
 		}
@@ -137,7 +149,6 @@ public class HttpClientUtil {
 
 	/** 
 	 * 创建SSL安全连接 
-	 * 
 	 * @return 
 	 */
 	private static SSLConnectionSocketFactory createSSLConnSocketFactory() {
@@ -172,6 +183,28 @@ public class HttpClientUtil {
 			e.printStackTrace();
 		}
 		return sslsf;
+	}
+
+	/**
+	 * 从实体类得到单表查询该实体的sql
+	 */
+	public static String getParams(final Object paramForm) {
+		Mirror<?> mirror = Mirror.me(paramForm);
+		Field[] fields = mirror.getFields();
+		List<String> fieldNames = Lists.transform(CollectionUtil.list(fields), new Function<Field, String>() {
+			@Override
+			public String apply(Field f) {
+				Object val = mirror.getValue(paramForm, f);
+				if (Util.isEmpty(val)) {
+					return null;
+				}
+				return f.getName() + "=" + val;
+			}
+		});
+
+		StringBuffer sb = new StringBuffer("?");
+		sb.append(Joiner.on("&").skipNulls().join(fieldNames));
+		return sb.toString();
 	}
 
 }
--- we-web-parent/web-linyun-airline/src/test/java/com/xiaoka/test/SabreAPITest.java
+++ we-web-parent/web-linyun-airline/src/test/java/com/xiaoka/test/SabreAPITest.java
@@ -12,13 +12,17 @@
  */
 package com.xiaoka.test;
 
+import org.apache.http.client.methods.HttpGet;
 import org.nutz.log.Log;
 import org.nutz.log.Logs;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import com.linyun.airline.common.sabre.SabreConfig;
 import com.linyun.airline.common.sabre.SabreTokenFactory;
 import com.linyun.airline.common.sabre.bean.SabreAccessToken;
+import com.linyun.airline.common.sabre.form.InstaFlightsSearchForm;
+import com.linyun.airline.common.util.HttpClientUtil;
 import com.uxuexi.core.common.util.Util;
 
 /**
@@ -44,7 +48,7 @@ public class SabreAPITest {
 	private static Log log = Logs.getLog(SabreAPITest.class);
 
 	@Test
-	public static void getAccessTokenTest() {
+	public void getAccessToken() {
 		int nullCnt = 0;
 		for (int i = 0; i < 20; i++) {
 			//从缓存中得到数据，由于我们没有设置过缓存，所以需要通过CacheLoader加载缓存数据
@@ -57,5 +61,60 @@ public class SabreAPITest {
 			}
 			Assert.assertEquals(nullCnt, 0);
 		}
+	}
+
+	/**
+	 * 1输入目的地直接查询(调试通过)
+	 * GET /v1/shop/flights/cheapest/fares/{destination}
+	 * 
+	 * 2复杂查询
+	 * A     /v1/shop/flights
+	 * B     /v1/shop/flights/tags/{tagid}
+	 * A接口指定参数enabletagging=true的时候会返回tagid
+	 * 
+	 * BJS   北京
+	 * SDY   悉尼
+	 */
+	@Test
+	public static void flightTo() {
+		String searchUrl = SabreConfig.test_environment + "/v1/shop/flights/cheapest/fares/SYD";
+		//		String searchUrl = test_environment + "/v1/shop/flights/cheapest/fares/BJS";
+
+		String result = null;
+		HttpGet httpget = new HttpGet(searchUrl);
+
+		SabreAccessToken accessToken = SabreTokenFactory.getAccessToken();
+		String token = accessToken.getAccess_token();
+		httpget.addHeader("Authorization", "Bearer " + token);
+
+		log.info("executing request " + httpget.getRequestLine());
+		result = HttpClientUtil.httpsGet(httpget);
+		log.info(result);
+	}
+
+	public static void instaFlightsSearch() {
+		InstaFlightsSearchForm form = new InstaFlightsSearchForm();
+		form.setOrigin("ATL");
+		form.setDestination("LAS");
+		form.setDeparturedate("2016-12-30");
+		form.setReturndate("2017-01-15");
+		form.setOffset(1);
+		form.setLimit(1);
+		String searchUrl = SabreConfig.test_environment + SabreConfig.INSTAL_FLIGHTS_URl
+				+ HttpClientUtil.getParams(form);
+		String result = null;
+		HttpGet httpget = new HttpGet(searchUrl);
+
+		SabreAccessToken accessToken = SabreTokenFactory.getAccessToken();
+		String token = accessToken.getAccess_token();
+		httpget.addHeader("Authorization", "Bearer " + token);
+
+		log.info("executing request " + httpget.getRequestLine());
+		result = HttpClientUtil.httpsGet(httpget);
+		log.info(result);
+	}
+
+	public static void main(String[] args) {
+		instaFlightsSearch();
 	}
 }
