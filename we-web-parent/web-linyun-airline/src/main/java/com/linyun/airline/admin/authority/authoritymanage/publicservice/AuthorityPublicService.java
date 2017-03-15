9ad53cc2b0eae0b74c5d85e49e86cd731f29ca0d
Cjb <Cjb@Cuijianbin>
2017-03-15 13:54:21 +0800

    修改公司添加模块，在添加完公司之后自动配上权限

--- we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/admin/authority/authoritymanage/publicservice/AuthorityPublicService.java
+++ we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/admin/authority/authoritymanage/publicservice/AuthorityPublicService.java
@@ -8,16 +8,14 @@ package com.linyun.airline.admin.authority.authoritymanage.publicservice;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
-import org.nutz.ioc.aop.Aop;
+import org.nutz.dao.Cnd;
 import org.nutz.ioc.loader.annotation.IocBean;
 
 import com.google.common.collect.Lists;
-import com.linyun.airline.admin.login.service.LoginService;
 import com.linyun.airline.common.enums.CompanyTypeEnum;
 import com.linyun.airline.entities.TCompanyEntity;
 import com.linyun.airline.entities.TCompanyFunctionMapEntity;
+import com.linyun.airline.forms.TCompanyAddForm;
 import com.uxuexi.core.common.util.Util;
 import com.uxuexi.core.web.base.service.BaseService;
 
@@ -30,18 +28,18 @@ import com.uxuexi.core.web.base.service.BaseService;
 public class AuthorityPublicService extends BaseService<TCompanyFunctionMapEntity> {
 
 	//新增公司后给公司配置功能
-	@Aop("txDb")
-	public boolean companyFunction(final HttpSession session) {
+	public boolean companyFunction(TCompanyAddForm addForm) {
 		//通过session获取公司的id
-		TCompanyEntity company = (TCompanyEntity) session.getAttribute(LoginService.USER_COMPANY_KEY);
-		Long companyId = company.getId();//得到公司的id
+		//TCompanyEntity company = (TCompanyEntity) session.getAttribute(LoginService.USER_COMPANY_KEY);
+		long adminId = addForm.getAdminId();//得到公司管理员的id
 		//根据公司id查询出是上游公司还是代理商
-		TCompanyEntity fetchType = dbDao.fetch(TCompanyEntity.class, companyId);
+		TCompanyEntity fetchType = dbDao.fetch(TCompanyEntity.class, Cnd.where("adminId", "=", adminId));
+		long companyId = fetchType.getId();//得到公司id
 		String comType = fetchType.getComType();//得到公司类型
 		int parseComType = Integer.parseInt(comType);//类型转换
 		//上游公司功能ID
 		int[] function = { 43, 44, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 68,
-				69, 75, 76, 81, 84, 85, 86, 87, 88 };
+				69, 75, 81, 84, 85, 86, 87, 88 };
 		List<TCompanyFunctionMapEntity> functionList = Lists.newArrayList();
 		if (parseComType == CompanyTypeEnum.UPCOMPANY.intKey()) {
 			for (int i = 0; i < function.length; i++) {
