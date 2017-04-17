679860c7ae7b85173688dfe978d3dc6cc3c28f58
Administrator <Administrator@PC-201212030907>
2017-04-17 10:42:17 +0800

    细节改动

---
+++ we-web-parent/web-linyun-airline/src/test/java/com/xiaoka/itext/BankTest.java
@@ -0,0 +1,20 @@
+/**
+ * BankTest.java
+ * com.xiaoka.itext
+ * Copyright (c) 2017, 北京科技有限公司版权所有.
+*/
+
+package com.xiaoka.itext;
+
+/**
+ * TODO(这里用一句话描述这个类的作用)
+ * <p>
+ * TODO(这里描述这个类补充说明 – 可选)
+ *
+ * @author   朱晓川
+ * @Date	 2017年4月17日 	 
+ */
+public class BankTest {
+
+}
+
