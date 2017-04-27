46388d9d5d820d620345e024aa3c4fe984d8b4db
PengH <PengH@DESKTOP-KNEENBS>
2017-04-27 09:29:00 +0800

    客户管理 联系电话  可填写座机号

--- we-web-parent/web-linyun-airline/src/main/webapp/admin/receivePayment/inland/inlandPay.js
+++ we-web-parent/web-linyun-airline/src/main/webapp/admin/receivePayment/inland/inlandPay.js
@@ -117,6 +117,7 @@ function initPayDataTable(){
 		            		$(".checkBoxPayAll").prop("checked", false);
 
 		            	}
+		            	autoHighLoad($(this));
 		            	return '显示第 '+start+' 至 '+end+' 条结果，共'+total+' 条 (每页显示 '+max+' 条)'
 		            }
 
@@ -312,6 +313,7 @@ function initPayEdDataTable(){
         		$(".checkBoxPayAll").prop("checked", false);
 
         	}
+        	autoHighLoad($(this));
         	return '显示第 '+start+' 至 '+end+' 条结果，共'+total+' 条 (每页显示 '+max+' 条)'
         }
         }],
