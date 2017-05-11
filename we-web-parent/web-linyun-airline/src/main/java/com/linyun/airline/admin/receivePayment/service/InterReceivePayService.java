9b5d0fb23fbe32adde7017b13488f2c370c9b8b1
PengH <PengH@DESKTOP-KNEENBS>
2017-05-11 15:57:17 +0800

    会计 国际付款检索

--- we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/admin/receivePayment/service/InterReceivePayService.java
+++ we-web-parent/web-linyun-airline/src/main/java/com/linyun/airline/admin/receivePayment/service/InterReceivePayService.java
@@ -555,7 +555,7 @@ public class InterReceivePayService extends BaseService<TPayEntity> {
 		Cnd cnd = Cnd.NEW();
 		SqlExpressionGroup group = new SqlExpressionGroup();
 		group.and("ci.shortName", "LIKE", "%" + name + "%").or("uo.ordersnum", "LIKE", "%" + name + "%")
-				.or("ci.linkMan", "LIKE", "%" + name + "%").or("pi.PNR", "LIKE", "%" + name + "%");
+				.or("ci.linkMan", "LIKE", "%" + name + "%").or("pii.PNR", "LIKE", "%" + name + "%");
 		if (!Util.isEmpty(name)) {
 			cnd.and(group);
 		}
