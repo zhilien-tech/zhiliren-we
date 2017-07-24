4eb0ca456c82235289bf081c7186a4505ce09bef
PengH <PengH@DESKTOP-KNEENBS>
2017-07-24 12:23:35 +0800

    template 002

---
+++ we-web-parent/web-template/src/main/java/com/xiaoka/template/forms/SRoleFunctionMapForm.java
@@ -0,0 +1,46 @@
+package com.xiaoka.template.forms;
+
+import java.io.Serializable;
+
+import lombok.Data;
+
+import org.nutz.dao.Cnd;
+import org.nutz.dao.SqlManager;
+import org.nutz.dao.Sqls;
+import org.nutz.dao.sql.Sql;
+
+import com.uxuexi.core.db.util.EntityUtil;
+import com.uxuexi.core.web.form.SQLParamForm;
+import com.xiaoka.template.entities.SRoleFunctionMapEntity;
+
+@Data
+public class SRoleFunctionMapForm implements SQLParamForm, Serializable {
+	private static final long serialVersionUID = 1L;
+	/**主键*/
+	private Integer id;
+
+	/**角色id*/
+	private Integer roleId;
+
+	/**功能id*/
+	private Integer functionId;
+
+	@Override
+	public Sql sql(SqlManager sqlManager) {
+		/**
+		 * 默认使用了当前form关联entity的单表查询sql,如果是多表复杂sql，
+		 * 请使用sqlManager获取自定义的sql，并设置查询条件
+		 */
+		String sqlString = EntityUtil.entityCndSql(SRoleFunctionMapEntity.class);
+		Sql sql = Sqls.create(sqlString);
+		sql.setCondition(cnd());
+		return sql;
+	}
+
+	private Cnd cnd() {
+		Cnd cnd = Cnd.NEW();
+		//TODO 添加自定义查询条件（可选）
+
+		return cnd;
+	}
+}
